c2c8c8093706b2a4a2ed90a7828b955427913518
BiwBong1706 <bewrockgame1@gmail.com>
2026-09-12 22:42:58 +0700

    Update Layout.jsx

--- expense-frontend/src/components/Layout.jsx
+++ expense-frontend/src/components/Layout.jsx
@@ -1,8 +1,10 @@
-//expense-frontend/src/components/Layout.jsx
+// expense-frontend/src/components/Layout.jsx
+
 import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
+import BottomNav from "./BottomNav";
 import api from "../api/axios";
 
 function Layout() {
@@ -23,18 +25,30 @@ function Layout() {
   };
 
   return (
-    <div className="flex h-screen bg-gray-100">
-      <Sidebar open={open} setOpen={setOpen} user={user} />
+    <div className="flex min-h-screen bg-gray-100">
+      {/* Sidebar */}
+      <Sidebar
+        open={open}
+        setOpen={setOpen}
+        user={user}
+      />
 
-      <div className="flex flex-1 flex-col overflow-hidden">
-        <Navbar setOpen={setOpen} user={user} />
+      {/* Main */}
+      <div className="flex min-w-0 flex-1 flex-col">
+        <Navbar
+          setOpen={setOpen}
+          user={user}
+        />
 
-        <main className="flex-1 overflow-y-auto bg-gray-100 p-6">
+        <main className="flex-1 overflow-y-auto bg-gray-100 p-6 pb-24 lg:pb-6">
           <div className="mx-auto w-full max-w-7xl">
             <Outlet />
           </div>
         </main>
       </div>
+
+      {/* Bottom Navigation สำหรับหน้าจอเล็ก */}
+      <BottomNav />
     </div>
   );
 }
